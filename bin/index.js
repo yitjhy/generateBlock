0d0833df9abec4b43688ba224fbc57ae8ca0af69
yitjhy@126.com <yitjhy@126.com>
2022-03-03 01:11:20 +0800

    init

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,8 @@
 #! /usr/bin/env node
 const shell = require('shelljs');
+const { cat } = require("shelljs");
+const $ = require("gogocode");
+const { writeFileSync } = require("fs");
 
 const copyFolder = 'bin';
 const argv = process.argv;
@@ -18,12 +21,41 @@ const generateBlock = () => {
             shell.cd(`../`);
             shell.mv([copyFolder], fileName);
             shell.rm('-rf', `${dirPath}`);
+            writeInParseFile(fileName)
         } else {
             console.log('Exit code:', code);
             console.log('Program output:', stdout);
             console.log('Program stderr:', stderr);
         }
     });
+}
+
+const ToUpperCase = str => {
+    let res = '';
+    if (str.indexOf('-') !== -1) {
+        const arr = str.split('-');
+        res = arr.reduce((pre, cur) => {
+            pre += cur.slice(0,1).toUpperCase() +cur.slice(1);
+            return pre
+        }, '');
+    } else {
+        res = str.slice(0,1).toUpperCase() +str.slice(1);
+    }
+    return res
+}
+
+const writeInParseFile = fileName => {
+    const targetContent = cat('./index.jsx').stdout;
+    if (targetContent) {
+        const newContent = $(targetContent).replace(`<UiFlag />`, `<${ToUpperCase(fileName)} />`)
+            .root()
+            .before(`import ${ToUpperCase(fileName)} from './${fileName}'; \n`)
+            .generate()
+        writeFileSync('./index.jsx', newContent, 'utf-8');
+        console.log('模块插入成功');
+    } else {
+        console.log('当前目录下无index.jsx文件，无法向其插入代码');
+    }
 }
 
 if (gitUrl) {
